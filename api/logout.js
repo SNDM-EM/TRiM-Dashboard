f0ebf95bf74ace7015996acd6c829f4bff8a0d01
SNDM-EM <pzfcykzsy7@privaterelay.appleid.com>
2026-04-01 00:12:45 +0100

    Delete api directory

--- api/logout.js
+++
@@ -1,6 +0,0 @@
-const { clearAuthCookie } = require('../lib/auth');
-
-module.exports = async (_req, res) => {
-  clearAuthCookie(res);
-  res.json({ ok: true });
-};
--- api/me.js
+++
@@ -1,7 +0,0 @@
-const { requireAuth } = require('../lib/auth');
-
-module.exports = async (req, res) => {
-  const user = requireAuth(req, res);
-  if (!user) return;
-  res.json({ user });
-};
